304de2f2de4fe12a95edf19444bda82b8ed5da6b
Jhonier <jhoniergarzon68@gmail.com>
2023-05-11 09:59:06 +0200

    ajuste-pagina

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+imagenesDado = [
+  "dado1.png",
+  "dado2.png",
+  "dado3.png",
+  "dado4.png",
+  "dado5.png",
+  "dado6.png",
+];
+
+dadoImg = document.getElementById('dado-img');
+
 function lanzarDado() {
-    if (animacion) return;
-    animacion = setInterval(() => {
-      dadoElem.textContent = dado[Math.floor(Math.random() * 6)];
-    }, 100);
-    setTimeout(() => {
-      clearInterval(animacion);
-      const resultado = Math.floor(Math.random() * 6) + 1;
-      resultados.push(resultado);
-      resultadosElem.innerHTML = resultados.map(r => `<li>${r}</li>`).join("");
-      dadoElem.textContent = "";
-      dadoImg.src = `dado${resultado}.png`;
-      animacion = null;
-    }, 1000);
-  }
-  
+  resultado = Math.floor(Math.random() * 6) + 1;
+  dadoImg.src = imagenesDado[resultado-1];
+  document.getElementById('dado').innerText = `Has sacado un ${resultado}!`;
+  historial = document.getElementById('resultados');
+  li = document.createElement('li');
+  li.innerText = `Resultado: ${resultado}`;
+  historial.appendChild(li);
+}
+
+botonLanzar = document.getElementById('lanzar');
+botonLanzar.addEventListener('click', lanzarDado);
+
+botonHistorial = document.getElementById('historial');
+botonHistorial.addEventListener('click', () => {
+  historial = document.getElementById('resultados');
+  historial.style.display = historial.style.display === 'none' ? 'block' : 'none';
+});
